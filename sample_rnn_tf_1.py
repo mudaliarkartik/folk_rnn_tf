79685c02cc2560517c2e17203a8215ce962adc42
mudaliarkartik <mudaliarkartik@yahoo.com>
2020-03-09 12:37:32 +0100

    Update

--- sample_rnn_tf_1.py
+++ sample_rnn_tf_1.py
@@ -9,23 +9,42 @@ import importlib
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
-from keras.layers import LSTM
+from keras.layers import LSTM, Embedding
 from keras import optimizers
 from keras.layers import Activation
 
 import argparse
 
+#config 
+one_hot = True
+embedding_size = 256  # is ignored if one_hot=True
+num_layers = 3
+rnn_size = 512
+dropout = 0.2
+
+learning_rate = 0.003
+learning_rate_decay_after = 20
+learning_rate_decay = 0.97
+
+batch_size = 64
+max_epoch = 100
+grad_clipping = 5
+validation_fraction = 0.05
+validate_every = 100  # iterations
+
+save_every = 1  # epochs
+
 parser = argparse.ArgumentParser()
-parser.add_argument('metadata_path')
+#parser.add_argument('metadata_path',type=str, default= "metadata_pickle_99.pkl")
 parser.add_argument('--rng_seed', type=int, default=42)
 parser.add_argument('--temperature', type=float, default=1.0)
-parser.add_argument('--ntunes', type=int, default=1)
+parser.add_argument('--ntunes', type=int, default=15)
 parser.add_argument('--seed')
 parser.add_argument('--terminal', action="store_true")
 
 args = parser.parse_args()
 
-metadata_path = args.metadata_path
+#metadata_path = args.metadata_path
 rng_seed = args.rng_seed
 temperature = args.temperature
 ntunes = args.ntunes
@@ -33,99 +52,46 @@ seed = args.seed
 
 def create_network(network_input, n_vocab):
     """ create the structure of the neural network """
-    rms_opt = optimizers.rmsprop(lr=config.learning_rate, decay=config.learning_rate_decay, clipvalue=config.grad_clipping)
+    rms_opt = optimizers.RMSprop(lr=learning_rate, clipvalue=grad_clipping)
     model = Sequential()
+    model.add(Embedding(input_dim=n_vocab, output_dim=n_vocab, embeddings_initializer='Identity'))
     model.add(LSTM(
-        config.rnn_size,
-        input_shape=(network_input.shape[1], network_input.shape[2]),
+        rnn_size,
+        input_shape =(1,),
         return_sequences=True
     ))
-    model.add(Dropout(config.dropout))
-    model.add(LSTM(config.rnn_size, return_sequences=True,))
-    model.add(Dropout(config.dropout))
-    model.add(LSTM(config.rnn_size))
-    model.add(Dropout(config.dropout))
+    model.add(Dropout(dropout))
+    model.add(LSTM(rnn_size,return_sequences=True))
+    model.add(Dropout(dropout))
+    model.add(LSTM(rnn_size))
+    model.add(Dropout(dropout))
     model.add(Dense(n_vocab))
     model.add(Activation('softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer=rms_opt)
-    model.summary()
+    model.compile(loss='sparse_categorical_crossentropy', optimizer=rms_opt)
+    #model.summary()
 
     # Load the weights to each node
-    model.load_weights('weights-improvement-04-3.2188-bigger.hdf5')
+    model.load_weights('weights.hdf5')
 
     return model
 
-def generate_notes(model, network_input, pitchnames, n_vocab):
-    start = np.random.randint(0, len(network_input)-1)
 
-    int_to_note = dict((number, note) for number, note in enumerate(pitchnames))
-
-    pattern = network_input[start]
-    prediction_output = []
-
-    # generate 500 notes
-    for note_index in range(500):
-        prediction_input = np.reshape(pattern, (1, len(pattern), 1))
-        prediction_input = prediction_input / float(n_vocab)
-
-        prediction = model.predict(prediction_input, verbose=0)
-
-        index = np.argmax(prediction)
-        result = int_to_note[index]
-        prediction_output.append(result)
-
-        pattern.append(index)
-        pattern = pattern[1:len(pattern)]
-
-    return prediction_output
-
-
-with open('data/tokens', 'rb') as f:
+with open('metadata_pickle_99.pkl','rb') as f:
     metadata = pickle.load(f)
-
-config = importlib.import_module('configurations.%s' % metadata['configuration'])
-
-# samples dir
-if not os.path.isdir('samples'):
-        os.makedirs('samples')
-target_path = "samples/%s-s%d-%.2f-%s.txt" % (
-    metadata['experiment_id'], rng_seed, temperature, time.strftime("%Y%m%d-%H%M%S", time.localtime()))
 
 token2idx = metadata['token2idx']
 idx2token = dict((v, k) for k, v in token2idx.items())
 vocab_size = len(token2idx)
 
-temp_tunes = metadata['tunes']
+x = np.zeros((ntunes,100),dtype='int32')
 
-print(vocab_size)
+model = create_network(x,vocab_size)
 
-
-sequence_length = 500
-network_input = []
-output = []
-for i in range(0, len(temp_tunes) - sequence_length, 1):
-    sequence_in = temp_tunes[i:i + sequence_length]
-    sequence_out = temp_tunes[i + sequence_length]
-    network_input.append([token2idx[char] for char in sequence_in])
-    output.append(token2idx[sequence_out])
-
-n_patterns = len(network_input)
-normalized_input = np.reshape(network_input, (n_patterns, sequence_length, 1))
-normalized_input = normalized_input / float(vocab_size)
-
-model = create_network(normalized_input,vocab_size)
-
-prediction_output = generate_notes(model, network_input, token2idx, vocab_size)
-
-print(prediction_output)
-
-'''
 start_idx, end_idx = token2idx['<s>'], token2idx['</s>']
 
 rng = np.random.RandomState(rng_seed)
 vocab_idxs = np.arange(vocab_size)
 
-# Converting the seed passed as an argument into a list of idx
 seed_sequence = [start_idx]
 if seed is not None:
     for token in seed.split(' '):
@@ -134,12 +100,14 @@ if seed is not None:
 for i in range(ntunes):
     sequence = seed_sequence[:]
     while sequence[-1] != end_idx:
-        next_itoken = rng.choice(vocab_idxs, p=model.predict(np.array([sequence], dtype='int32')))
+        temp = model.predict(np.array([sequence], dtype='int32'))
+        temp = np.reshape(temp, (vocab_size))
+        next_itoken = rng.choice(vocab_idxs, p=temp)
         sequence.append(next_itoken)
 
     abc_tune = [idx2token[j] for j in sequence[1:-1]]
     if not args.terminal:
-        f = open(target_path, 'a+')
+        f = open('samples.txt', 'a+')
         f.write('X:' + repr(i) + '\n')
         f.write(abc_tune[0] + '\n')
         f.write(abc_tune[1] + '\n')
@@ -150,8 +118,3 @@ for i in range(ntunes):
         print(abc_tune[0])
         print(abc_tune[1])
         print(' '.join(abc_tune[2:]) + '\n')
-
-
-if not args.terminal:
-    print('Saved to '+target_path)
-'''
--- train_rnn_tf_1.py
+++ train_rnn_tf_1.py
@@ -4,147 +4,250 @@ import time
 import logger
 import pickle
 import importlib
+import six
+import random
+from collections import defaultdict
+from random import shuffle
 
 import numpy as np
 import tensorflow as tf
-from data_iter import DataIterator
 import matplotlib.pyplot as plt
 
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, LSTM, Activation, Bidirectional, Flatten, Reshape
+from keras.layers import Dense, Dropout, LSTM, Activation,Reshape, Embedding, TimeDistributed
 from keras import utils
-from keras.callbacks import ModelCheckpoint
-from keras.utils import np_utils
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras.utils import np_utils, Sequence
 from keras import optimizers
 
-def train_network(network_input, network_output, vocab_size):
-    """ Train a Neural Network to generate music """
+#config 
+one_hot = True
+embedding_size = 256  # is ignored if one_hot=True
+num_layers = 3
+rnn_size = 512
+dropout = 0.2
+
+learning_rate = 0.003
+learning_rate_decay_after = 20
+learning_rate_decay = 0.97
+
+batch_size = 64
+max_epoch = 100
+grad_clipping = 5
+validation_fraction = 0.05
+validate_every = 100  # iterations
+
+save_every = 10  # epochs
+
+class DataIterator(object):
     
-    model = create_network(network_input, vocab_size)
+    def __init__(self, tune_lens, tune_idxs, batch_size, random_lens=False):
+        self.batch_size = batch_size
+        self.ntunes = len(tune_lens)
+        self.tune_idxs = tune_idxs
 
-    train(model, network_input, network_output)
+        self.len2idx = defaultdict(list)
+        for k, v in zip(tune_lens, tune_idxs):
+            self.len2idx[k].append(v)
+
+        self.random_lens = random_lens
+        self.rng = np.random.RandomState(42)
+    
+    def __iter__(self):
+        if self.random_lens:
+            for batch_idxs in self.__iter_random_lens():
+                yield np.int32(batch_idxs)
+        else:
+            for batch_idxs in self.__iter_homogeneous_lens():
+                yield np.int32(batch_idxs)
+    
+    def __iter_random_lens(self):
+        available_idxs = np.copy(self.tune_idxs)
+        while len(available_idxs) >= self.batch_size:
+            rand_idx = self.rng.choice(range(len(available_idxs)), size=self.batch_size, replace=False)
+            yield available_idxs[rand_idx]
+            available_idxs = np.delete(available_idxs, rand_idx)
+
+    def __iter_homogeneous_lens(self):
+        for idxs in six.itervalues(self.len2idx):#.itervalues():
+            self.rng.shuffle(idxs)
+
+        progress = defaultdict(int)
+        available_lengths = list(self.len2idx.keys())
+
+        batch_idxs = []
+        b_size = self.batch_size
+
+        get_tune_len = lambda: self.rng.choice(available_lengths)
+        k = get_tune_len()
+
+        while available_lengths:
+            batch_idxs.extend(self.len2idx[k][progress[k]:progress[k] + b_size])
+            progress[k] += b_size
+            if len(batch_idxs) == self.batch_size:
+                yield batch_idxs
+                batch_idxs = []
+                b_size = self.batch_size
+                k = get_tune_len()
+            else:
+                b_size = self.batch_size - len(batch_idxs)
+                i = available_lengths.index(k)
+                del available_lengths[i]
+                if not available_lengths:
+                    break
+                if i == 0:
+                    k = available_lengths[0]
+                elif i >= len(available_lengths) - 1:
+                    k = available_lengths[-1]
+                else:
+                    k = available_lengths[i + self.rng.choice([-1, 0])]
 
 
-def create_network(network_input, n_vocab):
+def create_network(x, n_vocab):
     """ create the structure of the neural network """
-    rms_opt = optimizers.rmsprop(lr=config.learning_rate, decay=config.learning_rate_decay, clipvalue=config.grad_clipping)
+    rms_opt = optimizers.RMSprop(lr=learning_rate, clipvalue=grad_clipping)
     model = Sequential()
+    model.add(Embedding(input_dim=n_vocab, output_dim=n_vocab, embeddings_initializer='Identity'))
     model.add(LSTM(
-        config.rnn_size,
-        input_shape=(network_input.shape[1], network_input.shape[2]),
+        rnn_size,
+        input_shape =(100,),
         return_sequences=True
     ))
-    model.add(Dropout(config.dropout))
-    model.add(LSTM(config.rnn_size, return_sequences=True,))
-    model.add(Dropout(config.dropout))
-    model.add(LSTM(config.rnn_size))
-    model.add(Dropout(config.dropout))
+    model.add(Dropout(dropout))
+    model.add(LSTM(rnn_size,return_sequences=True))
+    model.add(Dropout(dropout))
+    model.add(LSTM(rnn_size))
+    model.add(Dropout(dropout))
     model.add(Dense(n_vocab))
     model.add(Activation('softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer=rms_opt)
+    model.compile(loss='sparse_categorical_crossentropy', optimizer=rms_opt)
     model.summary()
 
     return model
 
-def train(model, network_input, network_output):
-    """ train the neural network """
-    filepath = "./metadata/weights-improvement-{epoch:02d}-{val_loss:.4f}-bigger.hdf5"
-    checkpoint = ModelCheckpoint(
-        filepath,
-        monitor='val_loss',
-        verbose=0,
-        save_best_only=True,
-        mode='auto'
-    )
-    callbacks_list = [checkpoint]
+data_path = "data/allabcwrepeats_parsed"
+with open(data_path, 'r', encoding='utf-8') as f:
+    data = f.read()
+    
+def remove_title(tune):
+    return ('\n').join(tune.split('\n')[1:])
 
-    history = model.fit(network_input, network_output, epochs=config.max_epoch, batch_size=config.batch_size, validation_split=config.validation_fraction ,callbacks=callbacks_list)
+tunes = data.split('\n\n')
 
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('model loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'val'], loc='upper left')
-    plt.savefig('./output/new_loss.png')
+# Remove all the titles to reduce vocab size
+tunes = [remove_title(tune) for tune in tunes]
 
-# create batch
+tokens_set = set('\n\n'.join(tunes).split())
+
+#tokens_set = set(data.split())
+start_symbol, end_symbol = '<s>', '</s>'
+tokens_set.update({start_symbol, end_symbol})
+
+idx2token = sorted(list(tokens_set)) # needs to be sorted to be the same after reloading the vocab
+vocab_size = len(idx2token)
+print('vocabulary size:', vocab_size)
+token2idx = dict(zip(idx2token, range(vocab_size)))
+
+start_symbol_id = token2idx['<s>']
+end_symbol_id = token2idx['</s>']
+
+
+tunes = [[token2idx[c] for c in [start_symbol] + t.split() + [end_symbol]] for t in tunes]
+
+# set maximum length to 500
+tunes = [t for t in tunes if len(t) <= 100]
+
+tunes.sort(key=lambda x: len(x), reverse=True)
+ntunes = len(tunes)
+
+tune_lens = np.array([len(t) for t in tunes])
+max_len = max(tune_lens)
+
+
+nvalid_tunes = ntunes * validation_fraction
+nvalid_tunes = int(batch_size * max(1, np.rint(
+    nvalid_tunes / float(batch_size))))  # round to the multiple of batch_size
+
+rng = np.random.RandomState(42)
+valid_idxs = rng.choice(np.arange(ntunes), nvalid_tunes, replace=False)
+
+ntrain_tunes = ntunes - nvalid_tunes
+train_idxs = np.delete(np.arange(ntunes), valid_idxs)
+
+print('n tunes:', ntunes)
+print('n train tunes:', ntrain_tunes)
+print('n validation tunes:', nvalid_tunes)
+print('min, max length', min(tune_lens), max(tune_lens))
+
 def create_batch(idxs):
     max_seq_len = max([len(tunes[i]) for i in idxs])
-    x = np.zeros((config.batch_size, max_seq_len), dtype='float32')
-    mask = np.zeros((config.batch_size, max_seq_len - 1), dtype='float32')
+    x = np.zeros((batch_size, max_seq_len), dtype='float32')
+    mask = np.zeros((batch_size, max_seq_len - 1), dtype='float32')
     for i, j in enumerate(idxs):
         x[i, :tune_lens[j]] = tunes[j]
         mask[i, : tune_lens[j] - 1] = 1
     return x, mask
 
 
-if __name__ == '__main__':
+temp_tunes = np.zeros((ntunes, max(tune_lens)), dtype='float32')
+length = max(map(len, tunes))
+temp_tunes=np.array([xi+[None]*(length-len(xi)) for xi in tunes], dtype='float32')
 
-    if len(sys.argv) < 3:
-        sys.exit("Usage: train_rnn.py <configuration_name> <train data filename>")
+model = create_network(temp_tunes, vocab_size)
 
-    # data preparation
-    config_name = sys.argv[1]
-    data_path = sys.argv[2]
+train_data_iterator = DataIterator(tune_lens[train_idxs], train_idxs, batch_size, random_lens=False)
+valid_data_iterator = DataIterator(tune_lens[valid_idxs], valid_idxs, batch_size, random_lens=False)
 
-    config = importlib.import_module('configurations.%s' % config_name)
-    experiment_id = '%s-%s-%s' % (
-        config_name.split('.')[-1], os.path.basename(data_path.split('.')[0]),
-        time.strftime("%Y%m%d-%H%M%S", time.localtime()))
-    print(experiment_id)
+niter = 1
+losses = []
+val_losses = []
+train_batches_per_epoch = ntrain_tunes / batch_size
+max_niter = max_epoch * train_batches_per_epoch
+nvalid_batches = nvalid_tunes / batch_size
+prev_time = time.clock()
 
-    # metadata
-    if not os.path.isdir('metadata'):
-        os.makedirs('metadata')
-    metadata_target_path = 'metadata'
+for epoch in range(max_epoch):
+    for i, train_batch_idxs in enumerate(train_data_iterator):
+        x_batch, mask_batch = create_batch(train_batch_idxs)
 
-    # logs
-    if not os.path.isdir('logs'):
-            os.makedirs('logs')
-    sys.stdout = logger.Logger('logs/%s.log' % experiment_id)
-    sys.stderr = sys.stdout
+        history = model.fit(x=x_batch.T,y=x_batch[i+1],batch_size=batch_size,verbose=0)
+        
+        current_time = time.clock()
+        
+        train_loss = history.history['loss'][0]
+        
+        print('%d/%d (epoch %.3f) train_loss=%6.8f time/batch=%.2fs' % (niter, max_niter, niter / float(train_batches_per_epoch), train_loss , current_time - prev_time))
+        
+        prev_time = current_time
+        losses.append(train_loss)
+        niter += 1
 
-    # load data
-    with open(data_path, 'r') as f:
-        data = f.read()
+        if niter % validate_every == 0:
+            print("Validating")
+            for valid_batch_idx in valid_data_iterator:
+                x_batch, mask_batch = create_batch(valid_batch_idx)
 
-    # construct symbol set
-    tokens_set = set(data.split())
-    start_symbol, end_symbol = '<s>', '</s>'
-    tokens_set.update({start_symbol, end_symbol})
+                val_loss = model.evaluate(x=x_batch.T,y=x_batch[i+1],batch_size=batch_size,verbose=0)
 
-    # construct token to number dictionary
-    idx2token = list(tokens_set)
-    vocab_size = len(idx2token)
-    print('vocabulary size:', vocab_size)
-    token2idx = dict(zip(idx2token, range(vocab_size)))
-    tunes = data.split('\n\n')
-    del data
+                print("Val Loss:", val_loss)
+                val_losses.append(val_loss)
+            
 
-    # transcribe tunes from symbol to index
-    tunes = [[token2idx[c] for c in [start_symbol] + t.split() + [end_symbol]] for t in tunes]
-    tunes.sort(key=lambda x: len(x), reverse=True)
-    ntunes = len(tunes)
+    if (epoch + 1) % save_every == 0:
+        with open('metadata_pickle_'+ str(epoch) +'.pkl' , 'wb') as f:
+            pickle.dump({
+                'iters_since_start': niter,
+                'losses_train': losses,
+                'losses_eval_valid': val_losses,
+                'token2idx': token2idx
+            }, f)
+        print("Saved to metadata.pkl file")
 
-    tune_lens = np.array([len(t) for t in tunes])
-    max_len = max(tune_lens)
+print('Saving the weights')
+model.save_weights('weights.hdf5')
 
-    # tunes for validation
-    nvalid_tunes = ntunes * config.validation_fraction
-    nvalid_tunes = int(config.batch_size * max(1, np.rint(
-        nvalid_tunes / float(config.batch_size))))  # round to the multiple of batch_size
 
-    rng = np.random.RandomState(42)
-    valid_idxs = rng.choice(np.arange(ntunes), nvalid_tunes, replace=False)
 
-    # tunes for training
-    ntrain_tunes = ntunes - nvalid_tunes
-    train_idxs = np.delete(np.arange(ntunes), valid_idxs)
 
-    print('n tunes:', ntunes)
-    print('n train tunes:', ntrain_tunes)
-    print('n validation tunes:', nvalid_tunes)
-    print('min, max length', min(tune_lens), max(tune_lens))
-    
-    train_network(,vocab_size)
+
+
